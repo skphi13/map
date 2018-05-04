2bf5d11ca7dcc53b4057bbb3361aebe50a3428ef
Phu Phang <38512251+skphi13@users.noreply.github.com>
2018-05-03 23:59:26 -0500

    Add files via upload

---
+++ map/js/markers.js
@@ -0,0 +1,68 @@
+var myLocations = [
+    {
+        title: 'Franklin Barbecue',
+        lat: 30.2701190,
+        lng:  -97.7312730,
+        type: 'Restaurant'
+    },
+    {
+        title: 'Easy Tiger',
+        lat: 30.2658670,
+        lng: -97.7356870,
+        type: 'Restaurant'
+    },
+    {
+        title: 'Whole Foods Market',
+        lat: 30.2706070,
+        lng: -97.7532590,
+        type:  'Store'
+    },
+    {
+        title: 'Rio',
+        lat: 30.2700080,
+        lng: -97.7492210,
+        type:  'Bar'
+    },
+    {
+        title: 'The Roosevelt Room',
+        lat: 30.2676570,
+        lng: -97.7462960,
+        type: 'Bar'
+    },
+    {
+        title: 'Halcyon',
+        lat: 30.2670360,
+        lng: -97.7456350,
+        type: 'Restaurant'
+    },
+    {
+        title: 'Peter Pan Mini-Golf',
+        lat: 30.2605880,
+        lng: -97.7574970,
+        type: 'Entertainment'
+    },
+    {
+        title: 'Texas Capitol',
+        lat: 30.2747030,
+        lng: -97.7403450,
+        type: 'Entertainment'
+    },
+    {
+        title: 'Whislers',
+        lat: 30.2619990,
+        lng: -97.7226820,
+        type: 'Bar'
+    },
+    {
+        title: 'Hotel Vegas',
+        lat: 30.2634130,
+        lng: -97.7271590,
+        type: 'Bar'
+    },
+    {
+        title: 'Bangers Sausage House & Beer Garden',
+        lat: 30.2588770,
+        lng: -97.7383810,
+        type: 'Restaurant'
+    }
+]
